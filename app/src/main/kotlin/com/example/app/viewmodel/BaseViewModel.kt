54530634f1d2cd6958abf95c45f5156bef1bd35b
Aldyaz Nugroho <blurayaddict95@gmail.com>
2020-04-29 20:47:28 +0700

    Create base viewmodel

---
+++ app/src/main/kotlin/com/example/app/viewmodel/BaseViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.app.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+
+/**
+ * @author Aldyaz Nugroho on 29/04/20.
+ * https://github.com/aldyaz
+ */
+abstract class BaseViewModel : ViewModel() {
+}
