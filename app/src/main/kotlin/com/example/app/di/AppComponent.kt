eb625228c965cb856395593e2ebdf54d2ff543e4
Aldyaz Nugroho <blurayaddict95@gmail.com>
2020-04-29 16:51:03 +0700

    Create base DI

---
+++ app/src/main/kotlin/com/example/app/di/AppComponent.kt
@@ -0,0 +1,31 @@
+package com.example.app.di
+
+import com.example.app.MainApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.support.AndroidSupportInjectionModule
+
+
+/**
+ * @author Aldyaz Nugroho on 29/04/20.
+ * https://github.com/aldyaz
+ */
+
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        AndroidSupportInjectionModule::class,
+        MainModule::class
+    ]
+)
+interface AppComponent {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: MainApplication): Builder
+        fun build(): AppComponent
+    }
+
+    fun inject(application: MainApplication)
+}
