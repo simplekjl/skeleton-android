9d7f67df146bee1b1f5a5d7f699f208ff25b2c46
Aldyaz Nugroho <blurayaddict95@gmail.com>
2020-06-01 20:49:16 +0700

    Domain: Base

---
+++ domain/src/main/kotlin/com/example/domain/base/executor/ThreadExecutor.kt
@@ -0,0 +1,11 @@
+package com.example.domain.base.executor
+
+import java.util.concurrent.Executor
+
+
+/**
+ * @author Aldyaz Nugroho on 01/06/20.
+ * https://github.com/aldyaz
+ */
+
+interface ThreadExecutor : Executor
