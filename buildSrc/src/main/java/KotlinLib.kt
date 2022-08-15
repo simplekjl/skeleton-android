b51f64b48f41e2bb72067f173272e68347f04d34
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-08-15 18:08:26 +0200

    updating compose compiler version base on compatibility matrix

--- buildSrc/src/main/java/KotlinLib.kt
+++ buildSrc/src/main/java/KotlinLib.kt
@@ -1,7 +1,7 @@
 object KotlinLib {
 
     const val version = "1.6.21"
-    const val kotlinComposeCompilerExtVersion = "1.1.1"
+    const val kotlinComposeCompilerExtVersion = "1.2.0-rc02"
 
     const val classpath = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
     const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
