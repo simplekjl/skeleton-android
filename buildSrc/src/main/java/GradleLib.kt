b47c0ddf487f7d8a716997a7649a08650224a194
Jose Luis Crisostomo Sanchez <jose.sanchez@vivy.com>
2023-02-12 11:05:17 +0100

    update gradle-wrapper.properties and GradleLib.kt

--- buildSrc/src/main/java/GradleLib.kt
+++ buildSrc/src/main/java/GradleLib.kt
@@ -1,6 +1,6 @@
 object GradleLib {
 
-    private const val pluginVersion = "7.0.0"
+    private const val pluginVersion = "7.4.0"
 
     const val classpath = "com.android.tools.build:gradle:$pluginVersion"
 
